b3126aae39c73f40df1117df08d14fda2fb3f8a1
FFN2004 <55273251+FFN2004@users.noreply.github.com>
2019-12-12 10:44:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".menu");
-const navLinks = document.querySelector(".nav-open");
+const navLinks = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("opened");
+    console.log("hello")
 });
